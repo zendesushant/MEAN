032f8f95fc3193a3b3332c6bf8704c704ff871e4
zendesushant <sushantcz99@gmail.com>
2020-09-09 19:54:07 +0530

    Angular_to_node

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ const app=express();
 const port=process.env.PORT || 3000
 
 
+app.use(express.json());
 
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin,Authorization,Content-Type");
+    res.setHeader("Access-Control-Allow-Methods","GET,PUT,PATCH,DELETE,POST");
+    next()
+})
 app.post('/users',(req,resp)=>{
-
-    const user=new users({
-
-        name:"Zende",
-        age:23
-    })
+    const user=new users(req.body)
     user.save().then(()=>{
 
         resp.send(user)
